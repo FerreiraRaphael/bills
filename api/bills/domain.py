33a0dfaf366b7fb63b9e707816c6f29b9211cc4d
fmossri <chicobulhoes@gmail.com>
2024-04-02 22:57:39 -0300

    added main_tag handling into create_new_bills_from_csv

--- api/bills/domain.py
+++ api/bills/domain.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from libsql_client import Transaction
 
 from api.bills._m.insert_bill import insert_bill
+from api.bills._m.update_bills import update_main_tag
 from api.bills.model import Bill
 from api.bills.tags._m.insert_bills_tags import insert_bills_tags
 from api.bills.tags._m.insert_tag import insert_tag
@@ -19,8 +20,9 @@ async def create_new_bills_from_csv(t: Transaction, text: str):
     str_date_time = first_row["date"] + "T" + first_row["time"] + "Z"
     date = datetime.strptime(str_date_time, "%Y-%m-%dT%H:%M:%SZ")
     new_bill = Bill(name=first_row["name"], value=float(first_row["number"]), date=date)
-    new_bill_db_res = await insert_bill(t, new_bill)
-    bill_dict = new_bill_db_res[0].dict()
+    new_bill_db_result = await insert_bill(t, new_bill)
+    bill_dict = new_bill_db_result[0].dict()
+    main_tag = first_row["main_tag"]
 
     for tag in tags_list:
         tag_id = await check_tag(t, name=tag)
@@ -37,3 +39,13 @@ async def create_new_bills_from_csv(t: Transaction, text: str):
                 bill_id=int(bill_dict["id"]), tag_id=int(tag_dict["id"])
             )
             await insert_bills_tags(t, bill_tag_ids)
+
+    main_tag_id = await check_tag(t, main_tag)
+
+    if len(main_tag_id) != 0:
+        main_tag_dict = main_tag_id[0]
+        update_main_tag(t, int(main_tag_dict["id"]), int(bill_dict["id"]))
+    else:
+        new_tag = await insert_tag(t, Tag(name=main_tag))
+        tag_dict = new_tag[0].dict()
+        update_main_tag(t, int(tag_dict["id"]), int(bill_dict["id"]))
