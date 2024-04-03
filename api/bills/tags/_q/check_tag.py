2c14a96567dfc422641a0d3a827457cab9319be1
fmossri <chicobulhoes@gmail.com>
2024-04-02 21:05:43 -0300

    first version of create_new_bill_from_csv

--- api/bills/tags/_q/check_tag.py
+++ api/bills/tags/_q/check_tag.py
@@ -2,5 +2,5 @@ from libsql_client import Transaction
 
 
 async def check_tag(t: Transaction, name: str):
-    res = await t.execute("SELECT * FROM tags WHERE name = ?;", [name])
-    return len(res.rows) != 0
+    res = await t.execute("SELECT id FROM tags WHERE name = ?;", [name])
+    return [row.asdict() for row in res.rows]
