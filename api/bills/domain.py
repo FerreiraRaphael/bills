51e6f3759795e94702be45a2ce0815de8345c6e4
fmossri <chicobulhoes@gmail.com>
2024-04-04 00:55:17 -0300

    refac create_new_bills_from_csv to accept any number of bills

--- api/bills/domain.py
+++ api/bills/domain.py
@@ -13,44 +13,49 @@ from api.bills.tags.model import BillTag, Tag
 from api.logger import RequestLogger
 
 
+def treat_csv_row_data(row: dict):
+    tags_list = row["tags"].split(",")
+    tags_list = [tag.lstrip() for tag in tags_list]
+    str_date_time = row["date"] + "T" + row["time"] + ":00Z"
+    date = datetime.strptime(str_date_time, "%Y-%m-%dT%H:%M:%SZ")
+    number_float_to_int = int(float(row["number"]) * 100)
+    main_tag = row["main_tag"]
+    return tags_list, date, number_float_to_int, main_tag
+
+
 async def create_new_bills_from_csv(t: Transaction, log: RequestLogger, text: str):
     reader = csv.DictReader(text.splitlines())
-    first_row = next(reader)
-    tags_list = first_row["tags"].split(",")
-    str_date_time = first_row["date"] + "T" + first_row["time"] + "Z"
-    date = datetime.strptime(str_date_time, "%Y-%m-%dT%H:%M:%SZ")
-    number_float_to_int = int(float(first_row["number"]) * 100)
-    new_bill = Bill(name=first_row["name"], value=number_float_to_int, date=date)
-    new_bill_db_result = await insert_bill(t, log, new_bill)
-    bill_dict = new_bill_db_result[0].dict()
-    main_tag = first_row["main_tag"]
+    for row in reader:
+        tags_list, date, number_float_to_int, main_tag = treat_csv_row_data(row)
+        new_bill = Bill(name=row["name"], value=number_float_to_int, date=date)
+        new_bill_db_result = await insert_bill(t, log, new_bill)
+        bill_dict = new_bill_db_result[0].dict()
+        main_tag_id = await check_tag(t, log, main_tag)
 
-    for tag in tags_list:
-        tag_id = await check_tag(t, log, name=tag)
-        if len(tag_id) != 0:
-            tag_dict = tag_id[0]
-            bill_tag_ids = BillTag(
-                bill_id=int(bill_dict["id"]), tag_id=int(tag_dict["id"])
+        if len(main_tag_id) != 0:
+            main_tag_dict = main_tag_id[0]
+            await update_main_tag(
+                t, log, main_tag_id=main_tag_dict["id"], bill_id=bill_dict["id"]
             )
-            await insert_bills_tags(t, log, bill_tag_ids)
         else:
-            new_tag = await insert_tag(t, log, Tag(name=tag))
-            tag_dict = new_tag[0].dict()
-            bill_tag_ids = BillTag(
-                bill_id=int(bill_dict["id"]), tag_id=int(tag_dict["id"])
+            new_tag = await insert_tag(t, log, Tag(name=main_tag))
+            main_tag_dict = new_tag[0].dict()
+            await update_main_tag(
+                t, log, main_tag_id=main_tag_dict["id"], bill_id=bill_dict["id"]
             )
-            await insert_bills_tags(t, log, bill_tag_ids)
 
-    main_tag_id = await check_tag(t, log, main_tag)
-
-    if len(main_tag_id) != 0:
-        main_tag_dict = main_tag_id[0]
-        await update_main_tag(
-            t, log, main_tag_id=main_tag_dict["id"], bill_id=bill_dict["id"]
-        )
-    else:
-        new_tag = await insert_tag(t, log, Tag(name=main_tag))
-        main_tag_dict = new_tag[0].dict()
-        await update_main_tag(
-            t, log, main_tag_id=main_tag_dict["id"], bill_id=bill_dict["id"]
-        )
+        for tag in tags_list:
+            tag_id = await check_tag(t, log, name=tag)
+            if len(tag_id) != 0:
+                tag_dict = tag_id[0]
+                bill_tag_ids = BillTag(
+                    bill_id=int(bill_dict["id"]), tag_id=int(tag_dict["id"])
+                )
+                await insert_bills_tags(t, log, bill_tag_ids)
+            else:
+                new_tag = await insert_tag(t, log, Tag(name=tag))
+                tag_dict = new_tag[0].dict()
+                bill_tag_ids = BillTag(
+                    bill_id=int(bill_dict["id"]), tag_id=int(tag_dict["id"])
+                )
+                await insert_bills_tags(t, log, bill_tag_ids)
--- api/bills/domain_test.py
+++ api/bills/domain_test.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 import aiofiles
-import pytest
 from libsql_client import Transaction
 
 from api.bills._q.fetch_bills import FetchBillsParams, fetch_bills
@@ -11,7 +10,6 @@ from api.logger import RequestLogger
 csv_path = "api/bills/test.csv"
 
 
-@pytest.mark.skip(reason="no way of currently testing this")
 async def test_create_new_bills_from_csv(t: Transaction, log: RequestLogger):
     async with aiofiles.open(csv_path, "r") as file:
         data = await file.read()
@@ -19,18 +17,31 @@ async def test_create_new_bills_from_csv(t: Transaction, log: RequestLogger):
         bill_list = await fetch_bills(
             t, log, FetchBillsParams(join_main_tag=True, join_tags=True)
         )
-        assert len(bill_list) == 1
-        assert bill_list[0].name == "Uber* Trip"
+        assert len(bill_list) == 56
+        assert bill_list[0].name == "Pag*Bacuribebidas"
         assert bill_list[0].id == 1
-        assert bill_list[0].value == 2216
+        assert bill_list[0].value == 4350
         assert (
             datetime.strftime(bill_list[0].date, "%Y-%m-%dT%H:%M:%SZ")
-            == "2024-02-26T00:00:00Z"
+            == "2024-02-24T00:00:00Z"
         )
-        assert len(bill_list[0].tags) == 2
+        assert len(bill_list[0].tags) == 1
         assert bill_list[0].tags[0].name == "Meus gastos"
         assert bill_list[0].tags[0].id == 1
-        assert bill_list[0].tags[1].name == "uber"
-        assert bill_list[0].tags[1].id == 2
         assert bill_list[0].main_tag.name == "Meus gastos"
         assert bill_list[0].main_tag.id == 1
+
+        assert bill_list[35].name == "Amazon Prime Canais"
+        assert bill_list[35].id == 36
+        assert bill_list[35].value == 994
+        assert (
+            datetime.strftime(bill_list[35].date, "%Y-%m-%dT%H:%M:%SZ")
+            == "2024-03-02T00:00:00Z"
+        )
+        assert len(bill_list[35].tags) == 2
+        assert bill_list[35].tags[0].name == "fixo"
+        assert bill_list[35].tags[0].id == 4
+        assert bill_list[35].tags[1].name == "assinatura"
+        assert bill_list[35].tags[1].id == 5
+        assert bill_list[35].main_tag.name == "fixo"
+        assert bill_list[35].main_tag.id == 4
